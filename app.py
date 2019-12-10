7cae776933c9b7c4dc40e9128f9706473b01e4ec
Diyar Kudrat <dkudrat@gmail.com>
2019-12-10 12:48:42 -0800

    finished UI design

--- app.py
+++ app.py
@@ -6,3 +6,19 @@ app = Flask(__name__)
 @app.route('/')
 def homepage():
     return render_template('homepage.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/user')
+def user():
+    return render_template('user.html')
